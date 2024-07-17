a027a87087ddf41559908adfa5ecbfd072210abb
Dzikry Maulana Hakim <dzikry.maulana.hakim@gmail.com>
2024-07-17 17:22:35 +0700

    fix: seeder and create directory tenant

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $tenant = Tenant::create(['id' => 'central']);
-        $tenant->domains()->create(['domain' => config('app.central_domain', 'localhost')]);
+        $tenant->domains()->create(['domain' => config('app.central_domain')]);
 
         $user = CentralUser::create([
             'global_id' => Str::orderedUuid(),
